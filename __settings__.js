b3c68a05023fe1e9838e8c58620c60ccee76d284
Aleksander Cyniak <114753995+dscountergo@users.noreply.github.com>
2025-01-18 15:56:38 +0100

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2154133.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 211195701, 211195702, 211195700 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/211195719/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/211195723/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/211195714/1/ammo.js', 'preload' : true},
+];
